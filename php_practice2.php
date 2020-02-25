9a8eb32371c2e5db3836911707de2601eef13eb6
yamane hironobu <halfmostmusic@yahoo.co.jp>
2020-02-25 11:16:15 +0000

    a

--- php_practice2.php
+++ php_practice2.php
@@ -2,11 +2,13 @@
 
 $name="yamane hironobu";
     if ($name="yamane hironobu") {
-      echo "私は".$name."です";
+        echo "私は".$name."です";
     } else {
-      echo "あなたの名前ではありません";
+        echo "あなたの名前ではありません";
     }
 echo "\n";
+
+
 $total = 0;
 
     for ($i = 0; $i <= 10000; $i++) {
@@ -17,7 +19,7 @@ echo "\n";
 
 $fruits = array("banana", "apple", "orange","momo","suika");
 
-    foreach($fruits as $fruits){
+    foreach ($fruits as $fruits){
     echo $fruits;
     }
 echo "\n";  
@@ -28,7 +30,7 @@ $end = 100;
 
     for($i = $start; $i <= $end; $i++){
         if($i % 5 == 0){
-        echo $i;
+        echo $i; 
         }
     }    
-    echo "\n";
+echo "\n";
