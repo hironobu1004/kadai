bdfd1c87abd7e9204f99afdb4988cc0e1836aac2
yamane hironobu <halfmostmusic@yahoo.co.jp>
2020-02-25 11:19:48 +0000

    a

--- php_practice2.php
+++ php_practice2.php
@@ -12,7 +12,7 @@ echo "\n";
 $total = 0;
 
     for ($i = 0; $i <= 10000; $i++) {
-    $total += $i;
+        $total += $i;
     }
 echo $total;
 echo "\n";
@@ -20,7 +20,7 @@ echo "\n";
 $fruits = array("banana", "apple", "orange","momo","suika");
 
     foreach ($fruits as $fruits){
-    echo $fruits;
+        echo $fruits;
     }
 echo "\n";  
 
@@ -30,7 +30,7 @@ $end = 100;
 
     for($i = $start; $i <= $end; $i++){
         if($i % 5 == 0){
-        echo $i; 
+            echo $i; 
         }
     }    
 echo "\n";
